313fe5595bcbee6631969aeb712b95de0d5bc254
Prima Secondary Ramadhan <73151849+primasr@users.noreply.github.com>
2021-01-09 01:19:24 +0700

    Commit

--- comments/db.php
+++ comments/db.php
@@ -1,5 +1,11 @@
 <?php
-   $conn = mysqli_connect("localhost","root","","pusing-bareng");
+   // $servername = "127.0.0.1:3306";
+   // $username = "u652632623_prima_db";
+   // $password = "Tekfo-eclair02";
+   // $db = "u652632623_prima_db";
+
+   // $conn = mysqli_connect("localhost","root","","pusing-bareng");
+   $conn = mysqli_connect("127.0.0.1:3306","u652632623_prima_db","Tekfo-eclair02","u652632623_prima_db");
    $conn->set_charset('utf8mb4');
 
 
--- conn.php
+++ conn.php
@@ -1,9 +1,14 @@
 <?php
     
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $db = "pusing-bareng";
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $db = "pusing-bareng";
+
+    $servername = "127.0.0.1:3306";
+    $username = "u652632623_prima_db";
+    $password = "Tekfo-eclair02";
+    $db = "u652632623_prima_db";
 
     $conn = mysqli_connect($servername,$username,$password,$db);
 
